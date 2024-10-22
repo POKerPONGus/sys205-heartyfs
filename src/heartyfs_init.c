b6c08a1103223d35ef43561d2ec1f1cbdaa61c92
Sarutch Supaibulpipat (Pokpong) <8pokpong8@gmail.com>
2024-10-22 18:54:46 +0700

    feat: init root

--- src/heartyfs_init.c
+++ src/heartyfs_init.c
@@ -1,6 +1,7 @@
 #include "heartyfs.h"
 
-int main() {
+int main()
+{
     // Open the disk file
     int fd = open(DISK_FILE_PATH, O_RDWR);
     if (fd < 0) {
@@ -9,13 +10,16 @@ int main() {
     }
 
     // Map the disk file onto memory
-    void *buffer = mmap(NULL, DISK_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    void *buffer =
+        mmap(NULL, DISK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (buffer == MAP_FAILED) {
         perror("Cannot map the disk file onto memory\n");
         exit(1);
     }
-
     // TODO:
-
-    return 0;
+    struct IONode_HeartyFS root = {.name = "/", .type = TYPE_DIR_HeartyFS};
+    if (writeBlock_HeartyFS(buffer, &root, 0) == NULL) {
+        perror("Cannot map initialize root block\n");
+        exit(1);
+    }
 }
